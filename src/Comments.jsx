2c7c216800fbdf59155a2331b8cbd3ad9c9e4cac
bzz-debug <ben.sumner@hotmail.com>
2025-05-21 12:37:37 +0100

    adding completed exercise 9: delete comment

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -1,14 +1,39 @@
-function Comments({ comment }) {
+import { deleteComment } from "./api";
+import { useState } from "react";
+
+function Comments({ comment, loggedInUser }) {
+  const { comment_id } = comment;
+  const [isDeleted, setIsDeleted] = useState(false);
+
+  // console.log(comment_id);
+
+  const handleDelete = (event) => {
+    setIsDeleted(true);
+    deleteComment(comment_id);
+    console.log(comment);
+  };
+
   return (
-    <div className="comment-card">
-      <span>
-        <b>{comment.author}:</b>
-      </span>
-      <span> {comment.body}</span>
-      <p>
-        <u>Votes: {comment.votes}</u>
-      </p>
-    </div>
+    <>
+      {!isDeleted ? (
+        <div className="comment-card">
+          <span>
+            <b>{comment.author}:</b>
+          </span>
+          <span> {comment.body}</span>
+          <p>
+            <u>Votes: {comment.votes}</u>
+          </p>
+          {loggedInUser === comment.author ? (
+            <button onClick={handleDelete} id="delete-button">
+              Delete Comment
+            </button>
+          ) : null}
+        </div>
+      ) : (
+        <p>comment deleted!</p>
+      )}
+    </>
   );
 }
 export default Comments;

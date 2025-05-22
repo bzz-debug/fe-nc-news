32d1f96c4967cdff046dfb8970c9f1db47f0e2f3
bzz-debug <ben.sumner@hotmail.com>
2025-05-22 18:07:19 +0100

    adding completed exercise 10: filter by topic

--- src/ArticleList.jsx
+++ src/ArticleList.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import api, { getArticles } from './api';
 import { useState, useEffect } from 'react';
 import ArticleCards from './ArticleCards';
@@ -7,21 +7,40 @@ function ArticleList({ articleId, setArticleId }) {
   const [articleList, setArticleList] = useState([]);
   const [topicName, setTopicName] = useState('');
 
+  // const { nameOfTopic } = useParams();
+
+  function handleTopicChange(nameOfTopic) {
+    setTopicName(nameOfTopic);
+  }
+
   useEffect(() => {
     getArticles(topicName).then((result) => {
-      console.log(result.data.filteredArticles, topicName);
-      if (result.data.filteredArticles) {
-        setArticleList(result.data.filteredArticles);
+      console.log(result.data.articles, topicName);
+      if (result.data.articles) {
+        setArticleList(result.data.articles);
       }
     });
   }, [topicName]);
-  // this is not working properly, it is sometimes giving me the necessary information, but most of the time the console log shows as undefined and the page doesnt change??? Very confused about what is happening here, I am concerned its a problem with the backend. I implemented almost identical code in the marketplace sprint and it worked.
+
+  // I have eventually, after many hours, managed to get the URL to change when I filter by topic. However, I have had to hard code it in, as you can see below. Please let me know how I can do this more dynamically.
+
   return (
     <>
       <header>
-        <button onClick={() => setTopicName('cooking')}>Cooking</button>
-        <button onClick={() => setTopicName('coding')}>Coding</button>
-        <button onClick={() => setTopicName('football')}>Football</button>
+        <Link to="/articles/">
+          <button onClick={() => handleTopicChange('')}>All</button>
+        </Link>
+        <Link to="/articles/cooking">
+          <button onClick={() => handleTopicChange('cooking')}>Cooking</button>
+        </Link>
+        <Link to="/articles/coding">
+          <button onClick={() => handleTopicChange('coding')}>Coding</button>
+        </Link>
+        <Link to="/articles/football">
+          <button onClick={() => handleTopicChange('football')}>
+            Football
+          </button>
+        </Link>
       </header>
       <div id="article-wrapper">
         <h1 id="article-list-title">ALL ARTICLES</h1>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,41 @@
-import { useState, useEffect } from "react";
-import { Link } from "react-router";
-import api from "./api";
+import { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router';
+import api from './api';
+import { LogInContext } from './LoggedInUser';
 
 function Home() {
+  const { setLoggedInUser, loggedInUser, isLoggedIn } =
+    useContext(LogInContext);
+  const [user, setUser] = useState({});
+  console.log(user);
+
+  function handleLogin(event) {
+    event.preventDefault();
+    setLoggedInUser(user);
+  }
+
   return (
     <div className="category-buttons">
       <h1 className="title">Natter</h1>
-      <Link to={"/articles"}>
+      <label htmlFor="username-selection">Who are you: </label>
+      <form onSubmit={handleLogin}>
+        <select
+          name="username-selection"
+          onChange={(event) => {
+            setUser(event.target.value);
+          }}
+        >
+          <option value="tickle122">tickle122</option>
+          <option value="grumpy19">grumpy19</option>
+          <option value="happyamy2016">happyamy2016</option>
+          <option value="cooljmessy">cooljmessy</option>
+          <option value="weegembump">weegembump</option>
+          <option value="jessjelly">jessjelly</option>
+        </select>
+        <input type="submit"></input>
+      </form>
+      <h2>{isLoggedIn ? 'Welcome: ' + loggedInUser : 'Welcome: Guest'}</h2>
+      <Link to={'/articles'}>
         <button id="intro-button">Let's Gossip</button>
       </Link>
     </div>

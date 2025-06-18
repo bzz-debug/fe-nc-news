78be9684aa5c85f376136bb7b29feac2b590210a
bzz-debug <ben.sumner@hotmail.com>
2025-06-18 13:49:20 +0100

    pushing weird errors to this branch before git pulling from main DO NOT MERGE

--- src/ArticleList.jsx
+++ src/ArticleList.jsx
@@ -3,49 +3,32 @@ import {
   useNavigate,
   useParams,
   useSearchParams,
-} from 'react-router-dom';
-import api, { getArticles, getArticlesSorted } from './api';
-import { useState, useEffect, useContext } from 'react';
-import ArticleCards from './ArticleCards';
-import { LogInContext } from './LoggedInUser';
+} from "react-router-dom";
+import api, { getArticles, getArticlesSorted } from "./api";
+import { useState, useEffect, useContext } from "react";
+import ArticleCards from "./ArticleCards";
+import { LogInContext } from "./LoggedInUser";
 
 function ArticleList({ articleId, setArticleId }) {
   const [articleList, setArticleList] = useState([]);
-  const [topicName, setTopicName] = useState('');
+  const [topicName, setTopicName] = useState("");
   const [searchParams, setSearchParams] = useSearchParams();
   const [topicErr, setTopicErr] = useState(false);
   const { setLoggedInUser, loggedInUser, isLoggedIn, storedLoggedInUser } =
     useContext(LogInContext);
 
-  /*
-  
-  the error handling isnt working because i have two useEffects which are both working at the same time and conflicting with each other, so if I set error state in both of them, the browser doesnt know whether to rendor an error and will render the last state.
-
-  I need to combine the two into one useEffect function.
-
-  Jim advised that I should also handle the get requests with one function and pass it three params. 
-
-  I am thinking I could maybe just call the getArticles function and pass a ${query} variable to it, as well as the three params. This may then enable me to make dynamic get requests that cover all potential queries. This MIGHT then also let me handle errors dynamically, when the user puts in a dodgy URL on the main page 
-
-  */
-
   const { topic } = useParams();
   console.log(topic);
 
-  // function handleQuery(event) {
-  //   console.log(event.target.value);
-  // }
+  const sortBy = searchParams.get("sort_by");
 
-  const sortBy = searchParams.get('sort_by');
-  // console.log(value);
-  const orderBy = searchParams.get('order') || 'desc';
+  const orderBy = searchParams.get("order") || "desc";
   console.log(orderBy);
 
   useEffect(() => {
     if (topic) {
       getArticles(topic, sortBy, orderBy)
         .then((result) => {
-          console.log(result.data.articles, topic);
           if (result.data.articles) {
             setArticleList(result.data.articles);
           }
@@ -55,9 +38,8 @@ function ArticleList({ articleId, setArticleId }) {
           setTopicErr(err.response.data.message);
         });
     } else {
-      getArticles('', sortBy, orderBy)
+      getArticles("", sortBy, orderBy)
         .then((result) => {
-          console.log(result.data.articles, topic);
           if (result.data.articles) {
             setArticleList(result.data.articles);
           }
@@ -91,10 +73,10 @@ function ArticleList({ articleId, setArticleId }) {
             </div>
             <div id="article-wrapper">
               <span>
-                {' '}
+                {" "}
                 <h1 id="article-list-title">
-                  {topic ? topic.toUpperCase() : 'ALL'} ARTICLES
-                </h1>{' '}
+                  {topic ? topic.toUpperCase() : "ALL"} ARTICLES
+                </h1>{" "}
                 <form action="" id="sort-by-dropdown">
                   <label htmlFor="SORT BY">SORT BY</label>
                   <select
